a30bf61d6b193fe1630008aaf4009dde75f983bc
arjuna26 <97765080+arjuna26@users.noreply.github.com>
2024-12-19 21:55:59 -0600

    remove vite server

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    host: '0.0.0.0', // Allows access from the local network
-    port: 8000,      // You can specify a custom port (default is 5173)
-    open: true       // Optional: Automatically open the app in your default browser
-  }
-});
